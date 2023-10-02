4bc11cfc9a8c23231dc9122bd9e1a6944433d104
Alexandre VASSOR <avassor@student.42.fr>
2023-10-02 16:26:43 +0200

    1

--- src/bonus/ai/ai.c
+++ src/bonus/ai/ai.c
@@ -3,23 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ai.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgarcia <jgarcia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: avassor <avassor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 10:05:02 by corecaps          #+#    #+#             */
-/*   Updated: 2023/09/25 10:05:05 by corecaps         ###   ########.fr       */
+/*   Updated: 2023/10/02 15:28:52 by avassor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/cub3d.h"
 
-void nmi_ai(t_raydata *raydata) {
-	int grid_size_x = WIDTH / (raydata->map_width);
-	int grid_size_y = HEIGHT / (raydata->map_height);
-	for (int i = 0;i < SPRITENBR; i ++) {
-		t_int_point dest = bfs(raydata,(t_int_point){(int)(raydata->spr->sprite[i].x) , (int )(raydata->spr->sprite[i].y)},(t_int_point) {(int)(raydata->player->pos.x / grid_size_x),(int)(raydata->player->pos.y/grid_size_y)} );
+void	nmi_ai(t_raydata *raydata)
+{
+	int			i;
+	int			grid_size_x;
+	int			grid_size_y;
+	t_int_point	dest;
+
+	i = 0;
+	grid_size_x = WIDTH / (raydata->map_width);
+	grid_size_y = HEIGHT / (raydata->map_height);
+	while (i < SPRITENBR)
+	{
+		dest = bfs(raydata, (t_int_point){(int)(raydata->spr->sprite[i].x),
+				(int)(raydata->spr->sprite[i].y)}, (t_int_point)
+			{(int)(raydata->player->pos.x / grid_size_x),
+				(int)(raydata->player->pos.y / grid_size_y)});
 		if (dest.x == 0 && dest.y == 0)
-			continue;
-		raydata->spr->sprite[i].x += (dest.x - (int)raydata->spr->sprite[i].x) * 0.05;
-		raydata->spr->sprite[i].y += (dest.y - (int)raydata->spr->sprite[i].y) * 0.05;
+			continue ;
+		raydata->spr->sprite[i].x
+			+= (dest.x - (int)raydata->spr->sprite[i].x) * 0.05;
+		raydata->spr->sprite[i].y
+			+= (dest.y - (int)raydata->spr->sprite[i].y) * 0.05;
 	}
 }
